412d793fe773d50b5750e78374008e1d9d32b838
Aaron S. Wolf <aswolf@umich.edu>
2014-10-01 22:42:58 -0400

    Added two oracle tests for Tbls in Tange(2009,12)
     - Shows perfect recovery of PVT model tables for:
     - mgsio3 pv using power law gruneisen param Tange 2012
     - mgo using Tange gruneisen param Tange 2009
     - agreement up to last digit

---
+++ eos-hammer/eosLib/calcPressThermAddEos.m
@@ -0,0 +1,37 @@
+% calcPressThermAddEos - calc total press using additive thermal contribution
+function [P,KT,Cv,gam] = calcPressThermAddEos(V,T,T0,pColdEos,pHotEos,...
+        coldFunEos,hotFunEos,elecThermPressFun)
+
+    V0 = pColdEos(1);
+
+    [PCold,KTCold] = coldFunEos(V,pColdEos);
+    [PHot,KTHot,CvHot,gamHot] = hotFunEos(V,T,V0,T0,pHotEos);
+
+    if(isempty(elecThermPressFun))
+        Pelec = 0;
+        Cvelec = 0;
+        dPdT_Velec = 0;
+    else
+        % Assumes that electronic thermal pressure is independent of volume
+        [Pelec Cvelec dPdT_Velec] = elecThermPressFun(T);
+        % Check whether electronic heat capacity and thermal press derivs are
+        % calculated
+        if(isempty(Cvelec))
+            Cvelec = zeros(size(Pelec));
+        end
+        if(isempty(dPdT_Velec))
+            dPdT_Velec = zeros(size(Pelec));
+        end
+    end
+
+    dPdT_VHot = CvHot.*gamHot./V;
+
+    % Additive quantities are derivatives of energy terms
+    P  = PCold  + PHot + Pelec;
+    KT = KTCold + KTHot;
+    Cv = CvHot  + Cvelec;
+    % NOTE: dPdT_V adds linearly, so we must combine terms and then determine
+    % gamma after
+    dPdT_V = dPdT_VHot + dPdT_Velec;
+    gam = dPdT_V.*V./Cv;
+end
